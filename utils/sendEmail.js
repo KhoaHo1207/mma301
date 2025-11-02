3590f1bbe749c0654e94ea23cbd7ac46d0a35cf5
Thomas Ho <khoaho120703@gmail.com>
2025-11-03 01:05:50 +0700

    push

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,9 +1,8 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
